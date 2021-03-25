4dc57b8907adbff6443e2eb35104b3080eed50df
Nam Dinh <nam.dt247@gmail.com>
2021-03-25 22:37:46 +0700

    fix controller

--- Zeal-Institute/Areas/Admin/AdminAreaRegistration.cs
+++ Zeal-Institute/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace Zeal_Institute.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "Zeal_Institute.Areas.Admin.Controllers" }
             );
         }
     }
