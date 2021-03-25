ba876d6608590208a79cf13f417e27c91f8dd3fb
Nam Dinh <nam.dt247@gmail.com>
2021-03-25 21:23:24 +0700

    first commit

---
+++ Zeal-Institute/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zeal_Institute.Startup))]
+namespace Zeal_Institute
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
