4b8285fe39ca8eb2be8759e122b94668e86eb032
Nam Dinh <nam.dt247@gmail.com>
2021-04-12 16:25:07 +0700

    update admin page

--- Zeal-Institute/Areas/Admin/Controllers/HomeController.cs
+++ Zeal-Institute/Areas/Admin/Controllers/HomeController.cs
@@ -68,8 +68,12 @@ namespace Zeal_Institute.Areas.Admin.Controllers
         public ActionResult Index()
         {
             var role = roleManager.FindByName("Student").Users.First();
-            var startDate = DateTime.Now;
-            var endDate = DateTime.Now.AddDays(-30);
+            var dateNow = DateTime.Now;
+            var firstDayOfMonth = new DateTime(dateNow.Year, dateNow.Month, 1);
+            var startDate = dateNow;
+            var endDate = dateNow.AddDays(-29);
+
+            Debug.WriteLine(dateNow);
 
             var dataStudent = db.Users
                 .Where(u => u.Roles.Select(r => r.RoleId).Contains(role.RoleId))
@@ -79,26 +83,79 @@ namespace Zeal_Institute.Areas.Admin.Controllers
                 .ToDictionary(k => k.Key.ToString("dd-MM-yyyy"), k=>k.Count())
                 ;
 
+            // data student
+            var countStudent = db.Users
+                .Where(u => u.Roles.Select(r => r.RoleId).Contains(role.RoleId))
+                .Where(u => u.CreatedAt >= firstDayOfMonth)
+                .Where(u => u.CreatedAt <= dateNow)
+                .Count()
+                ;
+
+            // data batch
+            var countBatch = db.Batches
+                .Where(x => x.DateStart >= firstDayOfMonth)
+                .Where(x => x.DateStart <= dateNow)
+                .Count()
+                ;
+
+            // data revenue
+            var dataRevenue = db.Payments
+                .Where(x => x.PayDate >= firstDayOfMonth)
+                .Where(x => x.PayDate <= dateNow)
+                .Sum(x => x.AmountPaid)
+                ;
+
+            var dataFinancial = db.Payments
+                .Where(u => u.PayDate >= endDate)
+                .Where(u => u.PayDate <= startDate)
+                .GroupBy(x => x.PayDate)
+                .ToDictionary(k => k.Key.ToString("dd-MM-yyyy"), k => k.Sum(m => m.AmountPaid))
+                ;
+
+            Debug.WriteLine(JsonConvert.SerializeObject(dataFinancial));
+
+            ViewBag.countStudent = countStudent;
+            ViewBag.countBatch = countBatch;
+            ViewBag.dataRevenue = dataRevenue;
             ViewBag.StudentData = JsonConvert.SerializeObject(dataStudent);
+            ViewBag.FinancialData = JsonConvert.SerializeObject(dataFinancial);
+
             return View();
         }
 
-        public JsonResult StudentData()
+        [HttpGet]
+        public string StudentData(DateTime start, DateTime end)
         {
             var role = roleManager.FindByName("Student").Users.First();
-            var startDate = DateTime.Now;
-            var endDate = DateTime.Now.AddDays(-30);
+            var startDate = start != null ? start : DateTime.Now.AddDays(-29);
+            var endDate = end != null ? end : DateTime.Now;
 
             var dataStudent = db.Users
                 .Where(u => u.Roles.Select(r => r.RoleId).Contains(role.RoleId))
-                .Where(u => u.CreatedAt >= endDate)
-                .Where(u => u.CreatedAt <= startDate)
+                .Where(u => u.CreatedAt <= endDate)
+                .Where(u => u.CreatedAt >= startDate)
                 .GroupBy(x => x.CreatedAt)
                 .ToDictionary(k => k.Key.ToString("dd-MM-yyyy"), k => k.Count())
                 ;
 
-            ViewBag.StudentData = JsonConvert.SerializeObject(dataStudent);
-            return ViewBag.StudentData;
+            var data = JsonConvert.SerializeObject(dataStudent);
+            return data;
+        }
+
+        public string FinancialData(DateTime start, DateTime end)
+        {
+            var startDate = start != null ? start : DateTime.Now.AddDays(-29);
+            var endDate = end != null ? end : DateTime.Now;
+
+            var dataFinancial = db.Payments
+                .Where(u => u.PayDate <= endDate)
+                .Where(u => u.PayDate >= startDate)
+                .GroupBy(x => x.PayDate)
+                .ToDictionary(k => k.Key.ToString("dd-MM-yyyy"), k => k.Sum(m => m.AmountPaid))
+                ;
+
+            var data = JsonConvert.SerializeObject(dataFinancial);
+            return data;
         }
 
         public ActionResult Login(string returnUrl)
